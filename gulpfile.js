dcbdeab59a6def08796a2fc4625ef1516ea9f462
Brent Jackson <jacksonblack@gmail.com>
2014-10-18 10:35:01 -0400

    Add breakpoint styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,16 @@
 
 var gulp = require('gulp');
 var basswork = require('gulp-basswork');
+var rename = require('gulp-rename');
+var mincss = require('gulp-minify-css');
 
 gulp.task('default', function() {
   gulp.src('./index.css')
     .pipe(basswork())
+    .pipe(rename('flex-object.css'))
+    .pipe(gulp.dest('./css'))
+    .pipe(mincss())
+    .pipe(rename('flex-object.min.css'))
     .pipe(gulp.dest('./css'));
 });
 
